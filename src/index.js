563d4bd88451a969618a71e8e31845a244b29a9a
Polina Samovich <P-Samovich@yandex.by>
2023-02-19 16:18:03 +0300

    feat: complite towel-sore task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == undefined || matrix == []) {
+    return [];
+  }
+  for (let i = 0; i < matrix.length; i ++) {
+    if ((i + 1) % 2 === 0) {
+      matrix[i].reverse();
+    }
+    
+    
+  }
+  return matrix.flat();
 }
+
+
+// let arr = [
+//   [ 1, 2, 3 ],
+//   [ 4, 5, 6 ],
+//   [ 7, 8, 9 ],
+//  ]
+console.log([] == undefined)
